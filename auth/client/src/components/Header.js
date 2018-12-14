96be2e22fe954eff031e18dbb4fb562b89a2219a
Liran Harary <liran.harary@somoglobal.com>
2018-12-14 13:29:12 +0000

    Header: render relevant links based on login state

--- auth/client/src/components/Header.js
+++ auth/client/src/components/Header.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderLinks() {
+    return this.props.authenticated
+      ? (
+        <React.Fragment>
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature</Link>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/signin">Sign In</Link>
+        </React.Fragment>
+      )
+  }
+
   render() {
     return (
       <div>
         <Link to="/">Redux Auth</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Feature</Link>
+        {this.renderLinks()}
       </div>
     )
   }
 }
 
-export default Header
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated }
+}
+
+export default connect(mapStateToProps)(Header)
