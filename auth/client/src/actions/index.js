3d3b17d79ea235f1bfe02610972209ef2caf8bcd
Liran Harary <liran.harary@somoglobal.com>
2018-12-14 13:17:15 +0000

    Complete signin action creator

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -23,11 +23,11 @@ export const signout = () => {
 
 export const signin = (formProps, callback) => async dispatch => {
   try {
-    const response = await axios.post('http://localhost:3090/signup', formProps)
+    const response = await axios.post('http://localhost:3090/signin', formProps)
     dispatch({ type: AUTH_USER, payload: response.data.token })
     localStorage.setItem('token', response.data.token)
     callback()
   } catch (error) {
-    dispatch({ type: AUTH_ERROR, payload: 'Email in use' })
+    dispatch({ type: AUTH_ERROR, payload: 'Invalid login' })
   }
 }
